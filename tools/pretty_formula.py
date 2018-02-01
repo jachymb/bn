6a175bd989a661b6a946b8c24764781a126ba4ac
Jáchym Barvínek <jachymb@gmail.com>
2018-02-01 11:16:50 +0100

    m

--- tools/pretty_formula.py
+++ tools/pretty_formula.py
@@ -10,33 +10,32 @@ def and2l(s):
         return []
 
 def transform(model):
-    try:
-        values = [(x[:x.find('(')],  [int(z) for z in x[x.find('(')+1:-1].split(",")]) for x in str(model).strip().split(" ")]
-        nvars = int(dict(values)['nvars'][0])
-        maxterms = int(dict(values)['maxterms'][0])
+    values = [(x[:x.find('(')],  [int(z) for z in x[x.find('(')+1:-1].split(",")]) for x in str(model).strip().split(" ")]
+    nvars = int(dict(values)['nvars'][0])
+    maxterms = int(dict(values)['maxterms'][0])
 
-        nterms = {x[0]:x[1] for (l,x) in values if l == "nterms"}
-        table = [ [ [""]*nvars for _ in range(nterms[v])] if nterms[v] > 0 else "F" for v in range(nvars)]
-        for k, v in values:
-            if k == "pos":
-                a,b,c = v
-                table[a][c-1][b] = " " + str(b)
-            elif k == "neg":
-                a,b,c = v
-                table[a][c-1][b] = "~" + str(b)
+    nterms = {x[0]:x[1] for (l,x) in values if l == "nterms"}
+    table = [ [ [""]*nvars for _ in range(nterms[v])] if nterms[v] > 0 else "F" for v in range(nvars)]
+    for k, v in values:
+        if k == "pos":
+            a,b,c = v
+            table[a][c-1][b] = " " + str(b)
+        elif k == "neg":
+            a,b,c = v
+            table[a][c-1][b] = "~" + str(b)
 
-        return "\n".join(str(x) + " <- " + "\n   | ".join(" & ".join(filter(bool, term)) for term in formula if any(term)) for x,formula in enumerate(table))
+    return "\n".join(str(x) + " <- " + "\n   | ".join(" & ".join(filter(bool, term)) for term in formula if any(term)) for x,formula in enumerate(table))
 
-    except ValueError:
-        values = [(x[:x.find('(')],  x[x.find('(')+1:-1]) for x in str(model).strip().split(" ")]
-        nvars = int(dict(values)['nvars'][0])
-        table = [[] for _ in range(nvars)]
-        for k, v in values:
-            if k == 'used':
-                v, a = v.split(',', 1)
-                table[int(v)].append(and2l(a))
+def transform_pac(model):
+    values = [(x[:x.find('(')],  x[x.find('(')+1:-1]) for x in str(model).strip().split(" ")]
+    nvars = int(dict(values)['nvars'][0])
+    table = [[] for _ in range(nvars)]
+    for k, v in values:
+        if k == 'used':
+            v, a = v.split(',', 1)
+            table[int(v)].append(and2l(a))
 
-        return "\n".join(str(x) + " <- " + "\n   | ".join(" & ".join(filter(bool, term)) for term in formula if any(term)) for x,formula in enumerate(table))
+    return "\n".join(str(x) + " <- " + "\n   | ".join(" & ".join(filter(bool, term)) for term in formula if any(term)) for x,formula in enumerate(table))
 
 for line in sys.stdin:
     print(line, end="")
